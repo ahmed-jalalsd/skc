287b7596105d81887ce880237405323b5273ec9c
Ahmed Jalal <ahmed.dev.jalal@gmail.com>
2018-09-19 09:21:32 +0200

    add social medi icons to the footer

--- resources/views/_partials/footer/footer.blade.php
+++ resources/views/_partials/footer/footer.blade.php
@@ -4,14 +4,26 @@
     <div class="level-left">
       <div class="text">
         <p class="sub-title">Road 33/3 Khartoum, Sudan</p>
-        <p class="sub-title"><a href="tel:+1-303-499-7111">+1 (303) 499-7111</a> | Naser></p>
+        <span> | </span>
+        <p class="sub-title">
+          <a href="tel:+1-303-499-7111">+1 (303) 499-7111</a>
+          <span>|</span> Naser
+        </p>
       </div>
     </div>
 
     <div class="level-right">
       <div>
-        <a href="#"><i class="fab fa-facebook"></i></a>
-        <a href="#"><i class="fab fa-twitter-square"></i></a>
+        <a href="https://www.facebook.com/SKCsudan/" target="_blank">
+          <span class="icon m-r-10">
+            <img src="{{ asset('images/pages/fb.png') }}" alt="link to facebook">
+          </span>
+        </a>
+        <a href="#">
+          <span class="icon">
+            <img src="{{ asset('images/pages/twitter.png') }}" alt="link to facebook">
+          </span>
+        </a>
       </div>
     </div>
 
