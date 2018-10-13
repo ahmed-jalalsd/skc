9c6273aa6e2b25fe220fe8f695e500a7a9734339
Ahmed Jalal <ahmed.dev.jalal@gmail.com>
2018-10-13 10:37:40 +0200

    create dogs, results, show_entires tables again to add classes table

--- database/migrations/2018_10_13_083110_create_dogs_table.php
+++ database/migrations/2018_10_13_083110_create_dogs_table.php
@@ -15,8 +15,36 @@ class CreateDogsTable extends Migration
     {
         Schema::create('dogs', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('breed_id')->unsigned();
+            $table->integer('class_id')->unsigned();
+            $table->string('age')->nullable();
+            $table->string('color')->nullable();
+            $table->string('dog_name')->nullable();
+            $table->string('pedigree_no')->nullable();
+            $table->string('hair_type')->nullable();
+            $table->string('microchip_no')->nullable();
+            $table->string('tatto')->nullable();
+            $table->string('sex')->nullable();
+            $table->string('sir')->nullable();
+            $table->string('dam')->nullable();
+            $table->string('sir_pedigree_no')->nullable();
+            $table->string('dam_pedigree_no')->nullable();
+            $table->string('breeder')->nullable();
+            $table->string('owner')->nullable();
+            $table->string('owner_address')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('email')->unique();
+            $table->string('dog_images')->nullable();
+            $table->date('date_of_birth')->nullable();
             $table->timestamps();
+
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('breed_id')->references('id')->on('breeds')->onDelete('set NULL');
+            $table->foreign('class_id')->references('id')->on('classes');
         });
+
     }
 
     /**
---
+++ database/migrations/2018_10_13_083606_create_results_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('order');
+            $table->double('classification');
+
+            $table->integer('show_entries_id')->unsigned();
+            $table->foreign('show_entries_id')->references('id')->on('show_entries');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('results');
+    }
+}
