ea00134db8115e16f0937046ff4c4f9d9f0b670a
Ahmed Jalal <ahmed.dev.jalal@gmail.com>
2018-12-22 21:00:46 +0100

    changed the config/database with the online

--- config/database.php
+++ config/database.php
@@ -41,17 +41,15 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'database' => env('DB_DATABASE', 'bbbn111_skc'),
+            'username' => env('DB_USERNAME', 'dev'),
+            'password' => env('DB_PASSWORD', 'L%26UcqSq$+^'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => true,
-            'strict' => false,
             'engine' => null,
         ],
 
