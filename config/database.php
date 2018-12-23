e5a6bce137302996e3a1f74b9cb5db3c722b10db
Ahmed Jalal <ahmed.dev.jalal@gmail.com>
2018-12-23 01:37:21 +0100

    add database

--- config/database.php
+++ config/database.php
@@ -41,17 +41,15 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'database' => env('DB_DATABASE', 'skcsudan'),
+            'username' => env('DB_USERNAME', 'devsudan'),
+            'password' => env('DB_PASSWORD', '123456789@^&'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => true,
-            'strict' => false,
             'engine' => null,
         ],
 
